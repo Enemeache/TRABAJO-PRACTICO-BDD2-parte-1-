4cc2a142ab0a9ac940885233c937ef5483256941
Enemeache <110419986+Enemeache@users.noreply.github.com>
2022-09-12 20:09:04 -0300

    Add files via upload

--- Develop/6sp_AjusteHorasCargadas.sql
+++ Develop/6sp_AjusteHorasCargadas.sql
@@ -7,7 +7,7 @@ create procedure AjusteHorasCargadas(in emp_id int, in id_cli int, in pro_id int
         set hor_horas_dia = horas
         where emp_legajo = emp_id and month(hor_fecha) = mes and pro_id_proyecto = pro_id;
 		
-        call liquidacion_gastos_proyecto(id_cli, pro_id, mes);
+        call consulta_gastos_proyecto(id_cli, pro_id, mes);
         
     end;
     
